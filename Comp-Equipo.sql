cf40c28646ecf469f89945ad765fc255632381dc
tomas nieto rossi <tomasnieto249@gmail.com>
2024-09-05 11:44:30 -0300

    terminamos el codigo para crear las tablas

--- Comp-Equipo.sql
+++ Comp-Equipo.sql
@@ -1,11 +1,7 @@
 CREATE TABLE Comp_Equipo (
     ID INT PRIMARY KEY,
-    ID_Copa_de_la_Liga INT,
-    ID_Liga_Profesional INT,
-    ID_Copa_Argentina INT,
+    ID_Competicion INT,
     ID_Equipo INT,
-    FOREIGN KEY (ID_Copa_de_la_Liga) REFERENCES Copa_De_La_Liga(ID),
-    FOREIGN KEY (ID_Liga_Profesional) REFERENCES Liga_Profesional(ID),
-    FOREIGN KEY (ID_Copa_Argentina) REFERENCES Copa_Argentina(ID),
+    FOREIGN KEY (ID_Competicion) REFERENCES Competiciones(ID),
     FOREIGN KEY (ID_Equipo) REFERENCES Equipos(ID)
 );

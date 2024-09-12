ae184b12532a405d0b980a14e860a9dae4022537
tomasnieto1908 <164179694+tomasnieto1908@users.noreply.github.com>
2024-09-12 11:58:15 -0300

    Update partidos.sql

--- partidos.sql
+++ partidos.sql
@@ -2,13 +2,11 @@ CREATE TABLE Partidos (
    ID INT PRIMARY KEY,                        
    Local INT,                               
    Visitante INT,                             
-   Fecha INT,                               
-   ID_Equipo INT,                             
+   Fecha INT,                                                            
    ID_Competicion INT,                        
    Goles_Local INT,                           
    Goles_Visitante INT,                      
    CONSTRAINT FK_Local FOREIGN KEY (Local) REFERENCES Equipos(ID),        
-   CONSTRAINT FK_Visitante FOREIGN KEY (Visitante) REFERENCES Equipos(ID), 
-   CONSTRAINT FK_Equipo FOREIGN KEY (ID_Equipo) REFERENCES Equipos(ID),    
+   CONSTRAINT FK_Visitante FOREIGN KEY (Visitante) REFERENCES Equipos(ID),   
    CONSTRAINT FK_Competicion FOREIGN KEY (ID_Competicion) REFERENCES Competicion(ID) 
 );

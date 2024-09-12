ad8f85fb01e6ab64b3db49f2d72e4ac8fa824bcd
tomasnieto1908 <164179694+tomasnieto1908@users.noreply.github.com>
2024-09-12 11:57:42 -0300

    Update Comp-Equipo.sql

--- Comp-Equipo.sql
+++ Comp-Equipo.sql
@@ -3,5 +3,5 @@ CREATE TABLE Comp_Equipo (
     ID_Competicion INT,
     ID_Equipo INT,
     FOREIGN KEY (ID_Competicion) REFERENCES Competiciones(ID),
-    FOREIGN KEY (ID_Equipo) REFERENCES Equipos(ID)
+    FOREIGN KEY (ID_Equipo) REFERENCES Equipo(ID)
 );

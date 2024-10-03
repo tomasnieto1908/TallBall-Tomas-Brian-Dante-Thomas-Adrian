465cab5976c7dec51606aac28020dc939d9bd9fa
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-10-03 08:44:33 -0300

    Acomode un archivo que estaba mal de jugadores y lo pase donde corresponde

--- Comp-Equipo.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE comp_equipo (
-    ID INT PRIMARY KEY,
-    ID_Competicion INT,
-    ID_Equipo INT,
-    FOREIGN KEY (ID_Competicion) REFERENCES competiciones(ID),
-    FOREIGN KEY (ID_Equipo) REFERENCES equipo(ID)
-);
-
